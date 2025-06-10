22cb7160eadb419e7ef78e78510a6e99b649c5eb
iabdalkader <i.abdalkader@gmail.com>
2025-06-10 11:47:20 +0200

    stm32/lwip: Add custom config for N6.

--- ports/stm32/lwip_inc/lwipopts.h
+++ ports/stm32/lwip_inc/lwipopts.h
@@ -13,6 +13,25 @@
 // Include common lwIP configuration.
 #include "extmod/lwip-include/lwipopts_common.h"
 
+#ifdef STM32N6
+#undef MEM_SIZE
+#undef TCP_MSS
+#undef TCP_WND
+#undef TCP_SND_BUF
+#undef MEMP_NUM_TCP_SEG
+#undef MEM_ALIGNMENT
+
+#define MEM_SIZE                        (16 * 1024)
+#define TCP_MSS                         (1460)
+#define TCP_OVERSIZE                    (TCP_MSS)
+#define TCP_WND                         (8 * TCP_MSS)
+#define TCP_SND_BUF                     (8 * TCP_MSS)
+#define TCP_SND_QUEUELEN                (2 * (TCP_SND_BUF / TCP_MSS))
+#define TCP_QUEUE_OOSEQ                 (1)
+#define MEMP_NUM_TCP_SEG                (2 * TCP_SND_QUEUELEN)
+#define MEM_ALIGNMENT                   32
+#endif
+
 extern uint32_t rng_get(void);
 
 #endif // MICROPY_INCLUDED_STM32_LWIP_LWIPOPTS_H
